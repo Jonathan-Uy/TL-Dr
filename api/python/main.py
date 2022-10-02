031b83543955317c672a36e43a26ba886b20171c
Dephoh <dfoley3838@gmail.com>
2022-10-01 20:11:41 -0400

    Started on creation of text ripper

--- api/python/main.py
+++ api/python/main.py
@@ -1,1 +1,19 @@
-print('hello')
+# convert pdf files to text files, clean them, and return them
+
+import PyPDF2
+import os
+
+
+pdffileobj = input_file.read()
+
+pdfreader = PyPDF2.PdfFileReader(pdffileobj)
+
+numpages = pdfreader.numPages
+
+pageobj = pdfreader.getPage(x + 1)
+
+text = pageobj.extractText()
+
+file1 = open(r"../../samples/1.txt","a")
+
+file1.writelines(text)
