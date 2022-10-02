267f01bb8ac6372dc1e62c8029d11ce16d86705b
Dephoh <dfoley3838@gmail.com>
2022-10-01 22:47:55 -0400

    can rip text in 'text' variable

--- api/python/main.py
+++ api/python/main.py
@@ -4,7 +4,7 @@ import PyPDF2
 import os
 
 rel_path = os.path.realpath(os.path.join(os.path.dirname(__file__)\
-    , '..','..','samples','1.pdf'))
+    , '..','..','samples','2.pdf'))
 
 pdffileobj = open(rel_path,'rb')
 
@@ -12,13 +12,10 @@ pdfreader = PyPDF2.PdfFileReader(pdffileobj)
 
 numpages = pdfreader.numPages
 
-pageobj = pdfreader.getPage(1)
+pageobj = pdfreader.getPage(0)
 
 text = pageobj.extractText()
 
-file1 = open(rel_path,"r")
+print(text)
 
-print(file1)
-
-file1.close()
 
